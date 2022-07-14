c8a0dfcbb5c6c766cf65924e9aa511e235aba1a5
chromodyne <dsinitiere@outlook.com>
2022-07-14 13:25:52 -0700

    Began work on basic functionality and setup.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const markdownGenerator = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the name of your project?",
+    "Would you like a description?",
+    "Would you like a section for the deployed application?",
+    "Would you like to include a screenshot?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    //Begin asking user questions.
+    console.log("Hello world!");
+
+}
 
 // Function call to initialize app
 init();

6016f99a811a0a0f6a8fd30be77b5d18f11e628a
chromodyne <dsinitiere@outlook.com>
2022-07-15 11:22:19 -0700

    Begin working on license detection logic.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  //TODO: Add actual return values and finish up adding licenses.
+  switch (license) {
+
+    case "Apache 2.0 License":
+      return;
+
+    case "Boost":
+      return;
+    
+    case "BSD 3-Clause":
+      return;
+
+    case "BSD 2-Clause":
+      return;
+    
+    case "CreativeCommons 0":
+      return;
+
+    case "CreativeCommons ShareAlike 4.0 International":
+      return;
+
+    case "CreativeCommons Attribution-NonCommerical 4.0 International":
+      return;
+    
+    //GNU Licenses
+    case "GNU GPL 3.0":
+      return;
+
+    case "GNU GPL 2.0":
+      return;
+    
+    case "GNU AGPL 3.0":
+      return;
+
+    case "GNU LGPL 3.0":
+      return;
+  
+    case "GNU FDL 1.3":
+      return;
+
+    case "MIT":
+      return;
+
+
+
+    default:
+      return "";
+
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -10,6 +61,7 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
 
+  //Check which license received.
   switch(license) {
     case "GPL 3.0":
       break;

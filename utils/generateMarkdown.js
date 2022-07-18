3625fc9bea269fdc60e0853bf26ab8351fa213f4
chromodyne <dsinitiere@outlook.com>
2022-07-18 15:31:04 -0700

    Revamping license badge rendering to something less ridiculous.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,53 +3,69 @@
 
 function renderLicenseBadge(license) {
   
-  //TODO: Add actual return values and finish up adding licenses.
-  switch (license) {
+  //TODO: Delete this uneccessary bloat.
+  // switch (license) {
 
-    case "Apache 2.0 License":
-      return;
+  //   case "Apache 2.0 License":
+  //     return;
 
-    case "Boost":
-      return;
+  //   case "Boost":
+  //     return;
     
-    case "BSD 3-Clause":
-      return;
+  //   case "BSD 3-Clause":
+  //     return;
 
-    case "BSD 2-Clause":
-      return;
+  //   case "BSD 2-Clause":
+  //     return;
     
-    case "CreativeCommons 0":
-      return;
+  //   case "CreativeCommons 0":
+  //     return;
 
-    case "CreativeCommons ShareAlike 4.0 International":
-      return;
+  //   case "CreativeCommons ShareAlike 4.0 International":
+  //     return;
 
-    case "CreativeCommons Attribution-NonCommerical 4.0 International":
-      return;
+  //   case "CreativeCommons Attribution-NonCommerical 4.0 International":
+  //     return;
     
-    //GNU Licenses
-    case "GNU GPL 3.0":
-      return;
+  //   //GNU Licenses
+  //   case "GNU GPL 3.0":
+  //     return;
 
-    case "GNU GPL 2.0":
-      return;
+  //   case "GNU GPL 2.0":
+  //     return;
     
-    case "GNU AGPL 3.0":
-      return;
+  //   case "GNU AGPL 3.0":
+  //     return;
 
-    case "GNU LGPL 3.0":
-      return;
+  //   case "GNU LGPL 3.0":
+  //     return;
   
-    case "GNU FDL 1.3":
-      return;
+  //   case "GNU FDL 1.3":
+  //     return;
 
-    case "MIT":
-      return;
+  //   case "MIT":
+  //     return;
 
-    default:
-      return "";
+  //   default:
+  //     return "";
+
+  // }
+
+  //Check that the license is not "None" then render the badge. If it is "None" then return the empty string.
+  //TODO - Change license options in index.js to match the name directly so we can directly insert the name here
+  //to reduce bloat.
+  if (license !== "None") {
+
+    //We want to 
+    //Uses 
+    return `![License] https://img.shields.io/badge/License-${license}-blue.svg)`;
+
+  } else {
+
+    return;
 
   }
+
 }
 
 // TODO: Create a function that returns the license link
@@ -58,7 +74,7 @@ function renderLicenseLink(license) {
 
   //If LICENSE return link
   //ELSE return ""
-  if (license != "None") {
+  if (license !== "None") {
 
   } else {
     return "";
@@ -70,11 +86,6 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-}
-
-//Let's put our initial license check in here so we don't have to repeat ourselves.
-function checkLicense() {
 
 }
 

9a2aeae82f614dedf5568b849c4e00a85095db45
chromodyne <dsinitiere@outlook.com>
2022-07-18 19:22:23 -0700

    Fixed accidental misplacement of license rendering logic.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-
+//Generates a license badge depending on which license the user selected during inquiry.
 function renderLicenseBadge(license) {
   
   //Check that the license is not "None" then render the badge. If it is "None" then return the empty string.
@@ -19,11 +17,8 @@ function renderLicenseBadge(license) {
 
 }
 
-function renderLicenseLink(license) {
 
-  if (license !== "None") {
-    return generateLicenseText(license);
-  }
+function renderLicenseLink(license) {
 
 }
 
@@ -34,12 +29,15 @@ function generateLicenseText(license) {
   
   The license for this project is a ${license} license.
   `;
-
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+
+  if (license !== "None") {
+    return generateLicenseText(license);
+  }
 
 }
 
@@ -55,6 +53,8 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## Contents ##
+
+  ##
 
 `;
 }
